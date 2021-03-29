1b4a0b95ef967a5985d66523b2a2aeb3c10fd528
Rudi B. Svartveit <rudsvar@gmail.com>
2021-03-29 14:59:53 +0200

    Add Criterion benchmarks

---
+++ benches/criterion.rs
@@ -0,0 +1,40 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+
+fn random_string(length: usize) -> String {
+    let mut buf = String::with_capacity(length);
+    for _ in 0..length {
+        let c: char = rand::random();
+        buf.push(c);
+    }
+    buf
+}
+
+fn encoding_benchmark(c: &mut Criterion) {
+    let random_string = random_string(100_000);
+    c.bench_function("encode", |b| b.iter(|| huffman::encode(&random_string)));
+}
+
+fn decoding_benchmark(c: &mut Criterion) {
+    let random_string = random_string(100_000);
+    let encoded = huffman::encode(&random_string).unwrap();
+    c.bench_function("decode", |b| b.iter(|| huffman::decode(&encoded)));
+}
+
+fn encoding_decoding_benchmark(c: &mut Criterion) {
+    let random_string = random_string(100_000);
+    c.bench_function("encode-decode", |b| {
+        b.iter(|| {
+            let encoded = huffman::encode(&random_string)?;
+            huffman::decode(&encoded)
+        })
+    });
+}
+
+criterion_group! {
+    benches,
+    encoding_benchmark,
+    decoding_benchmark,
+    encoding_decoding_benchmark
+}
+
+criterion_main!(benches);
