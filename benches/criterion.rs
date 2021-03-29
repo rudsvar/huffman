149ce7f644a8789b0b454f2aa3ae401fd95c4a40
Rudi B. Svartveit <rudsvar@gmail.com>
2021-03-29 15:09:27 +0200

    Update benchmark sizes

--- benches/criterion.rs
+++ benches/criterion.rs
@@ -9,19 +9,21 @@ fn random_string(length: usize) -> String {
     buf
 }
 
+const INPUT_SIZE: usize = 100_000;
+
 fn encoding_benchmark(c: &mut Criterion) {
-    let random_string = random_string(100_000);
+    let random_string = random_string(INPUT_SIZE);
     c.bench_function("encode", |b| b.iter(|| huffman::encode(&random_string)));
 }
 
 fn decoding_benchmark(c: &mut Criterion) {
-    let random_string = random_string(100_000);
+    let random_string = random_string(INPUT_SIZE);
     let encoded = huffman::encode(&random_string).unwrap();
     c.bench_function("decode", |b| b.iter(|| huffman::decode(&encoded)));
 }
 
 fn encoding_decoding_benchmark(c: &mut Criterion) {
-    let random_string = random_string(100_000);
+    let random_string = random_string(INPUT_SIZE);
     c.bench_function("encode-decode", |b| {
         b.iter(|| {
             let encoded = huffman::encode(&random_string)?;
